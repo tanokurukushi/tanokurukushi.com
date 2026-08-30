c877292d43058806d1f700ece60de54035c3ccdf
TanokuRukushi <154442612+tanokurukushi@users.noreply.github.com>
2026-08-31 00:10:31 +0900

    add date format utility

---
+++ src/lib/format.ts
@@ -0,0 +1,7 @@
+/** Returns a date as a string value in `yyyy.mm.dd` format. */
+export function formatDate(date: Date): string {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const dd = String(date.getDate()).padStart(2, '0');
+  return `${yyyy}.${mm}.${dd}`;
+}
